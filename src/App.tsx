08846510ce90a735d719e792d0b0506ae167122d
Danilo Sousa <danilosousa.developer@gmail.com>
2022-10-13 11:54:05 -0300

    delete stories | config colors tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './styles/global.css';
 export function App() {
   return (
     <>
-        <h1 className="font-bold text-5xl text-violet-500">Hello world</h1>
-        <button className='bg-violet-500 font-medium px-4 py-2 text-white hover:bg-violet-900'>SEND</button>
+        <h1 className="font-bold text-2xl text-violet-500">Hello world</h1>
+        <button className='bg-marron-500 font-medium px-4 py-2 text-white hover:bg-marron-100'>SEND</button>
     </>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,28 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontSize: {
+      xs:14,
+      sm:16,
+      md:18,
+      lg:20,
+      xl:24,
+      '2xl':32,
+    },
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+
+      'marron-900': '#332105',
+      'marron-700': '#664714',
+      'marron-500': '#99702E',
+      'marron-300': '#CC9D52',
+      'marron-100': '#FFCE80'
+    },
+
     extend: {
       fontFamily:{
         sans: 'Nunito Sans, sans-serif'
